c9f6d8298ffd6943f0f92731f73309a6fa0a017a
Jinyoung Jang <jyjang@uengine.org>
2023-01-03 01:50:09 +0900

    commit

--- src/main/java/com/eventstorming/external/Relation.java
+++ src/main/java/com/eventstorming/external/Relation.java
@@ -1,6 +1,33 @@
 forEach: Relation
-fileName: test.java
-path: /
+fileName: {{target.namePascalCase}}Service_.java
+path: {{source.boundedContext.name}}/{{options.packagePath}}/external
+except: {{contexts.except}}
 ---
 
-dsadfsa
+package {{options.package}}
+
+{{#if fallback}}
+//<<< Resilency / Fallback
+@FeignClient(name = "{{target.boundedContext.name}}", url = "{{apiVariable target.boundedContext.name}}", fallback = {{target.namePascalCase}}ServiceImpl.class)
+//>>> Resilency / Fallback
+{{else}}
+@FeignClient(name = "{{target.boundedContext.name}}", url = "{{apiVariable target.boundedContext.name}}")
+{{/if}}
+public interface {{target.namePascalCase}}Service {
+ 
+    {{#ifContains "$.target._type" "View"}}
+    @RequestMapping(method= RequestMethod.GET, path="/{{target.namePlural}}/{{wrap commandValue.aggregate.keyFieldDescriptor.name}}")
+    public {{commandValue.aggregate.namePascalCase}} get{{commandValue.aggregate.namePascalCase}}(@PathVariable("{{commandValue.aggregate.keyFieldDescriptor.name}}") {{commandValue.aggregate.keyFieldDescriptor.className}} {{commandValue.aggregate.keyFieldDescriptor.name}});
+    {{/ifContains}}
+}
+
+
+
+<function>
+this.contexts.except = !((this.source._type.endsWith("Command") || this.source._type.endsWith("Policy")) && (this.target._type.endsWith("View") || this.target._type.endsWith("Aggregate")))
+ 
+if(!this.contexts.except){
+
+}
+ 
+</function>
