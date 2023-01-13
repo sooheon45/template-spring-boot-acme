e20dc90bb22aa25ba73270498d57d47b3e4a3247
Jinyoung Jang <jyjang@uengine.org>
2023-01-14 02:30:56 +0900

    commit

--- src/main/java/com/eventstorming/external/ExternalServiceImpl.java
+++ src/main/java/com/eventstorming/external/ExternalServiceImpl.java
@@ -1,35 +1,32 @@
-forEach: RelationCommandInfo
-fileName: {{commandValue.aggregate.namePascalCase}}ServiceImpl.java
-path: {{boundedContext.name}}/{{{options.packagePath}}}/external
+forEach: Relation
+fileName: {{target.aggregate.namePascalCase}}ServiceImpl.java
+path: {{source.boundedContext.name}}/{{options.packagePath}}/external
+except: {{contexts.except}}
+ifDuplicated: merge
 ---
+
 package {{options.package}}.external;
 
 import org.springframework.stereotype.Service;
 
-//<<< Resilency / Fallback
-@Service
-public class {{commandValue.aggregate.namePascalCase}}ServiceImpl implements {{commandValue.aggregate.namePascalCase}}Service {
-
-
-    /**
-     * Fallback
-     */
-    public {{commandValue.aggregate.namePascalCase}} get{{commandValue.aggregate.namePascalCase}}(Long id) {
-        {{commandValue.aggregate.namePascalCase}} {{commandValue.aggregate.nameCamelCase}} = new {{commandValue.aggregate.namePascalCase}}();
-        return {{commandValue.aggregate.nameCamelCase}};
-    }
+//<<< Resilency / Fallback 
+{{#ifContains "$.target._type" "View"}}
+{{#ifEquals target.dataProjection "query-for-aggregate"}}
+public class {{target.aggregate.namePascalCase}}ServiceImpl implements {{target.aggregate.namePascalCase}}Service {
+    public {{target.aggregate.namePascalCase}} {{camelCase target.name}}({{pascalCase target.name}}Query query){}
+{{/ifEquals}}
+{{/ifContains}}
 }
+
 //>>> Resilency / Fallback
 
-<function>
 
-  window.$HandleBars.registerHelper('log', function (aggregate) {
-//      console.log("template log:", aggregate)
-    return aggregate;
-  })
-
-  window.$HandleBars.registerHelper('wrap', function (exp) {
-    return '{'+exp+'}';
-  })
-
+<function> 
+ 
+this.contexts.except = !(this.value.fallback && (this.source._type.endsWith("Command") || this.source._type.endsWith("Policy")) && (this.target._type.endsWith("View") || this.target._type.endsWith("Aggregate")))
+ 
+if(!this.contexts.except){
+ 
+}
+ 
 </function>
