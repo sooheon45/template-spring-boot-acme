82b2a28877c96f9474ce17cb44b39a01592177c4
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-04-19 15:54:10 +0900

    Update Saga.java

--- src/main/java/com/eventstorming/saga/Saga.java
+++ src/main/java/com/eventstorming/saga/Saga.java
@@ -1,3 +1,8 @@
+forEach: Policy
+fileName: {{namePascalCase}}Saga.java
+path: {{boundedContext.name}}/{{{options.packagePath}}}/saga
+except: {{#isSaga}}false{{else}}true{{/isSaga}}
+---
 package {{options.package}}.saga;
 
 import {{options.package}}.config.kafka.KafkaProcessor;
@@ -23,3 +28,48 @@ public class {{namePascalCase}}Saga {
     {{/contexts.sagaEvents}}
 
 }
+
+<function>
+if(this.isSaga){
+    var eventByNames = []
+    var commandByNames = {}
+    this.outgoingCommandRefs.forEach(
+        commandRef => {
+            commandByNames[commandRef.name] = commandRef.value
+        }
+    )
+
+    var me = this;
+    var i = 1;
+    var maxSeq = 0;
+    this.incomingEventRefs.forEach(
+        eventRef => {
+            var nameNumberPart = eventRef.name.replace(/\D/g, "");
+    //        alert(nameNumberPart);
+            var sequence = parseInt(nameNumberPart); // i
+            if(sequence > maxSeq) maxSeq = sequence;
+
+            var commandSequence = sequence + 1;
+            eventByNames[sequence] = {
+                event: eventRef.value,
+                command: commandByNames[commandSequence],
+                compensateCommand: commandByNames[commandSequence+"'"],
+                isStartSaga: sequence ==1,
+                isEndSaga: false
+            };
+        }
+    )
+
+    eventByNames[maxSeq].isEndSaga = true;
+    //alert('x')
+    //alert(JSON.stringify(commandByNames))
+
+    this.contexts.sagaEvents = eventByNames; 
+
+
+}
+
+window.$HandleBars.registerHelper('except', function (fieldDescriptors) {
+    return (fieldDescriptors && fieldDescriptors.length == 0);
+});
+</function>
