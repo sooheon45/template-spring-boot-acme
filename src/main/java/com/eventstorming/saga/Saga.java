ac53361db4dd8984a0b147327c72b9839a6b1c24
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-04-27 12:13:27 +0900

    Update Saga.java

--- src/main/java/com/eventstorming/saga/Saga.java
+++ src/main/java/com/eventstorming/saga/Saga.java
@@ -87,7 +87,7 @@ public class {{namePascalCase}}Saga {
             .ifPresent({{../command.aggregate.nameCamelCase}} -> {
                 {{../command.namePascalCase}}Command {{../command.nameCamelCase}}Command = new {{../command.namePascalCase}}Command();
                 /* Logic */
-                {{#correlationGetSet event command}}{{/correlationGetSet}}
+                {{#correlationGetSet ../event ../command}}{{/correlationGetSet}}
                 
                 {{../command.aggregate.nameCamelCase}}.{{../command.nameCamelCase}}({{../command.nameCamelCase}}Command);
             });
