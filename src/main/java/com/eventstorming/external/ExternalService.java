dbc866316a8b6fb026e77a62dfff3fbef4ec4db5
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-05-02 16:58:15 +0900

    Update ExternalService.java

--- src/main/java/com/eventstorming/external/ExternalService.java
+++ src/main/java/com/eventstorming/external/ExternalService.java
@@ -6,6 +6,7 @@ ifDuplicated: merge
 ---
 package {{options.package}}.external;
 
+import {{options.package}}.domain.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
