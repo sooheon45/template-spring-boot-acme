7102d89dfe6e2e74db89216a0f2dec151107a346
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-05-10 11:19:12 +0900

    Update PolicyHandler.java

--- src/main/java/com/eventstorming/infra/PolicyHandler.java
+++ src/main/java/com/eventstorming/infra/PolicyHandler.java
@@ -1,154 +1,109 @@
-forEach: Policy
-fileName: {{namePascalCase}}Saga.java
-path: {{boundedContext.name}}/{{{options.packagePath}}}/saga
+forEach: BoundedContext
+fileName: PolicyHandler.java
+representativeFor: Policy
+path: {{name}}/{{{options.packagePath}}}/infra
 except: {{#isSaga}}false{{else}}true{{/isSaga}}
+mergeType: template
 ---
-package {{options.package}}.saga;
+package {{options.package}}.infra;
 
+import javax.naming.NameParser;
+import org.springframework.transaction.annotation.Transactional;
 import {{options.package}}.config.kafka.KafkaProcessor;
-import {{options.package}}.domain.*;
-import {{options.package}}.external.*;
-
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.stereotype.Service;
+import {{options.package}}.domain.*;
 
+//<<< Clean Arch / Inbound Adaptor
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class PolicyHandler{
+    {{#aggregates}}
+    @Autowired {{namePascalCase}}Repository {{nameCamelCase}}Repository;
+    {{/aggregates}}
 
-public class {{namePascalCase}}Saga {
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString, 
+                                @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+                                @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey){
+          /*
+          // Call port method with received messageKey to publish msg to the same partition. //
+          DomainClass.portMethod(eventString, new String(messageKey));
+          
+          // ,or //
+          new EventRaised(domain Obj).publishAfterCommit(new String(messageKey));
+          // manual Offset Commit. //
+          acknowledgment.acknowledge();  
+          */
+    }
 
+    {{#policies}}
+    {{^isSaga}}
+    {{#outgoing "ReadModel" .}}
     @Autowired
-    {{#boundedContext.aggregates}}
-        {{namePascalCase}}Repository {{nameCamelCase}}Repository;
-    {{/boundedContext.aggregates}}
-    {{#externalService boundedContext.aggregates contexts.sagaEvents}}{{/externalService}}
+    {{../../options.package}}.external.{{aggregate.namePascalCase}}Service {{aggregate.nameCamelCase}}Service;
 
-    
-    {{#contexts.sagaEvents}}
-    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='{{event.namePascalCase}}'")
-    public void whenever{{event.namePascalCase}}_{{../namePascalCase}}(@Payload {{event.namePascalCase}} {{event.nameCamelCase}}, 
+    {{/outgoing}}
+
+    {{#incoming "Event" .}}
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='{{namePascalCase}}'")
+    public void whenever{{namePascalCase}}_{{../namePascalCase}}(@Payload {{namePascalCase}} {{nameCamelCase}}, 
                                 @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
                                 @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) byte[] messageKey){
 
-        {{event.namePascalCase}} event = {{event.nameCamelCase}};
-        System.out.println("\n\n##### listener {{../namePascalCase}} : " + {{event.nameCamelCase}} + "\n\n");
+        {{namePascalCase}} event = {{nameCamelCase}};
+        System.out.println("\n\n##### listener {{../namePascalCase}} : " + {{nameCamelCase}} + "\n\n");
 
-    {{#compensateCommand}}
-        try {
-            {{../command.namePascalCase}}Command {{../command.nameCamelCase}}Command = new {{../command.namePascalCase}}Command();
-           /* Logic */
-            {{#todo ../description}}{{/todo}}
-            {{../command.aggregate.nameCamelCase}}Service.{{../command.nameCamelCase}}({{../command.nameCamelCase}}Command);
-        } catch (Exception e) {           
-            {{#if outgoingRelations}}
-            {{namePascalCase}}Command {{nameCamelCase}}Commad = new {{namePascalCase}}Command();
+        {{#../aggregateList}}
+        {{namePascalCase}}.{{../../nameCamelCase}}(event);        
+        {{/../aggregateList}}
 
-            {{aggregate.nameCamelCase}}Repository.findById(
-                // implement: Set the {{aggregate.namePascalCase}} Id from one of {{../event.namePascalCase}} event's corresponding property
-                event.getId()
-            )
-            .ifPresent({{aggregate.nameCamelCase}} -> {
-                {{aggregate.nameCamelCase}}.{{nameCamelCase}}({{nameCamelCase}}Commad);
-            });
-            {{else}}
-                {{aggregate.nameCamelCase}}Service.{{nameCamelCase}}(event.getId());
-            {{/if}}
+        {{#outgoing "Command" ..}}
+        {{#isExtendedVerb}}
+        {{namePascalCase}}Command {{nameCamelCase}}Command = new {{namePascalCase}}Command();
+        // implement:  Map command properties from event
 
-        }
-    {{else}}
-        /* Logic */
-        {{#todo ../description}}{{/todo}}
-        {{#command.outgoingRelations}}
-            {{#ifEquals source.aggregate.elementView.id target.aggregate.elementView.id}}
-            {{../command.aggregate.nameCamelCase}}Repository
-            .findById(
+        {{aggregate.nameCamelCase}}Repository.findById(
                 // implement: Set the {{aggregate.namePascalCase}} Id from one of {{../namePascalCase}} event's corresponding property
-                event.getId()
-            )
-            .ifPresent({{../command.aggregate.nameCamelCase}} -> {
-                {{../command.namePascalCase}}Command {{../command.nameCamelCase}}Command = new {{../command.namePascalCase}}Command();
-                {{../command.aggregate.nameCamelCase}}.{{../command.nameCamelCase}}({{../command.nameCamelCase}}Command);
-            });
-            {{else}}
-            {{command.namePascalCase}}Command {{../command.nameCamelCase}}Command = new {{../command.namePascalCase}}Command();
-            
-            {{command.nameCamelCase}}Command.setId(event.getId());
-            {{event.aggregate.nameCamelCase}}.{{command.nameCamelCase}}(updateStatusCommand);
-            {{/ifEquals}}
-        {{/command.outgoingRelations}}
-    {{/compensateCommand}}
+
+            ).ifPresent({{aggregate.nameCamelCase}}->{
+             {{aggregate.nameCamelCase}}.{{nameCamelCase}}({{nameCamelCase}}Command); 
+        });
+        {{else}}
+        {{aggregate.namePascalCase}} {{aggregate.nameCamelCase}} = new {{aggregate.namePascalCase}}();
+        {{aggregate.nameCamelCase}}Repository.save({{aggregate.nameCamelCase}});
+        {{/isExtendedVerb}}
+        {{/outgoing}}
+
+
+        {{#todo ../description}}{{/todo}}
 
         // Manual Offset Commit //
         acknowledgment.acknowledge();
+
     }
-    {{/contexts.sagaEvents}}
-    
+    {{/incoming}}
+    {{/isSaga}}
+    {{/policies}}
 }
+
+//>>> Clean Arch / Inbound Adaptor
+
 
 <function>
-if(this.isSaga){
-    var eventByNames = []
-    var commandByNames = {}
-    this.outgoingCommandRefs.forEach(
-        commandRef => {
-            commandByNames[commandRef.name] = commandRef.value
-        }
-    )
+window.$HandleBars.registerHelper('todo', function (description) {
 
-    var me = this;
-    var i = 1;
-    var maxSeq = 0;
-    this.incomingEventRefs.forEach(
-        eventRef => {
-            var nameNumberPart = eventRef.name.replace(/\D/g, "");
-    //        alert(nameNumberPart);
-            var sequence = parseInt(nameNumberPart); // i
-            if(sequence > maxSeq) maxSeq = sequence;
-
-            var commandSequence = sequence + 1;
-            eventByNames[sequence] = {
-                event: eventRef.value,
-                command: commandByNames[commandSequence],
-                compensateCommand: commandByNames[commandSequence+"'"],
-                isStartSaga: sequence ==1,
-                isEndSaga: false
-            };
-        }
-    )
-
-    eventByNames[maxSeq].isEndSaga = true;
-    //alert('x')
-    //alert(JSON.stringify(commandByNames))
-
-    this.contexts.sagaEvents = eventByNames; 
-
-
-}
-
-
-window.$HandleBars.registerHelper('externalService', function (aggregatesForBc, aggregates) {
-   var lists = [];
-   var str = ''
-   aggregatesForBc.forEach(function(selfAggregate){
-       if(aggregates){
-        aggregates.forEach(function(agg){
-           if(agg && agg.command){
-               if(agg.command.aggregate.name != selfAggregate.name){
-                 lists.push(agg.command.aggregate);
-               }
-           }
-          });
-       }
-    });
-    lists.forEach(function(agg){
-        str = str +`${agg.namePascalCase}Service ${agg.nameCamelCase}Service;`
-    })
-    return str;
-});
-
-window.$HandleBars.registerHelper('except', function (fieldDescriptors) {
-    return (fieldDescriptors && fieldDescriptors.length == 0);
+    if(description){
+        description = description.replaceAll('\n','\n\t\t// ')
+        return description = '// Comments // \n\t\t//' + description;
+    }
+     return null;
 });
 </function>
